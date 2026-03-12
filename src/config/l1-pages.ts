8fd2bd1df19f02076ea4e02ebad65e95446b6e26
snik-dev <ashton@snik.dev>
2026-03-12 16:08:29 -0700

    config: expand L1 pages to full 22-category list, correct AOR assignments, capitalize owner names

--- src/config/l1-pages.ts
+++ src/config/l1-pages.ts
@@ -5,44 +5,26 @@ export interface L1PageConfig {
 }
 
 export const L1_PAGES: L1PageConfig[] = [
-  {
-    label: 'Electronics',
-    url: 'https://www.mynavyexchange.com/browse/electronics/_/N-2540394923',
-    aorOwner: 'maddie',
-  },
-  {
-    label: 'Everyday Home',
-    url: 'https://www.mynavyexchange.com/browse/everyday-home/_/N-3389903099',
-    aorOwner: 'leigh',
-  },
-  {
-    label: 'Outdoor Home',
-    url: 'https://www.mynavyexchange.com/browse/outdoor-home/_/N-3084580992',
-    aorOwner: 'leigh',
-  },
-  {
-    label: 'Furniture',
-    url: 'https://www.mynavyexchange.com/browse/furniture/_/N-3183556638',
-    aorOwner: 'leigh',
-  },
-  {
-    label: 'Apparel',
-    url: 'https://www.mynavyexchange.com/browse/apparel/kids-apparel/_/N-469120406',
-    aorOwner: 'megan',
-  },
-  {
-    label: 'Military',
-    url: 'https://www.mynavyexchange.com/browse/military/_/N-1413539776',
-    aorOwner: 'daryl',
-  },
-  {
-    label: 'Shoes',
-    url: 'https://www.mynavyexchange.com/browse/shoes/_/N-2611111227',
-    aorOwner: 'megan',
-  },
-  {
-    label: 'Beauty',
-    url: 'https://www.mynavyexchange.com/browse/beauty/_/N-3719489590',
-    aorOwner: 'megan',
-  },
+  { label: 'Military', url: 'https://www.mynavyexchange.com/browse/military/_/N-1413539776', aorOwner: 'Maddie' },
+  { label: "Men's", url: 'https://www.mynavyexchange.com/browse/men-s/_/N-380633284', aorOwner: 'Megan' },
+  { label: "Women's", url: 'https://www.mynavyexchange.com/browse/women-s/_/N-459937891', aorOwner: 'Megan' },
+  { label: 'Kids', url: 'https://www.mynavyexchange.com/browse/apparel/kids-apparel/_/N-469120406', aorOwner: 'Megan' },
+  { label: 'Baby', url: 'https://www.mynavyexchange.com/browse/kids/_/N-816291448', aorOwner: 'Maddie' },
+  { label: 'Accessories', url: 'https://www.mynavyexchange.com/browse/accessories/_/N-3390284235', aorOwner: 'Daryl' },
+  { label: 'Shoes', url: 'https://www.mynavyexchange.com/browse/shoes/_/N-2611111227', aorOwner: 'Maddie' },
+  { label: 'Beauty', url: 'https://www.mynavyexchange.com/browse/beauty/_/N-3719489590', aorOwner: 'Maddie' },
+  { label: 'Personal Care', url: 'https://www.mynavyexchange.com/browse/personal-care/_/N-879452703', aorOwner: 'Maddie' },
+  { label: 'Electronics', url: 'https://www.mynavyexchange.com/browse/electronics/_/N-2540394923', aorOwner: 'Daryl' },
+  { label: 'Everyday Home', url: 'https://www.mynavyexchange.com/browse/everyday-home/_/N-3389903099', aorOwner: 'Maddie' },
+  { label: 'Furniture', url: 'https://www.mynavyexchange.com/browse/furniture/_/N-3183556638', aorOwner: 'Maddie' },
+  { label: 'Outdoor Home', url: 'https://www.mynavyexchange.com/browse/outdoor-home/_/N-3084580992', aorOwner: 'Maddie' },
+  { label: 'Sports, Fitness & Outdoors', url: 'https://www.mynavyexchange.com/browse/fitness/_/N-1055229383', aorOwner: 'Megan' },
+  { label: 'Office & School Supplies', url: 'https://www.mynavyexchange.com/browse/office-school-supplies/_/N-3714973793', aorOwner: 'Daryl' },
+  { label: 'Luggage & Travel', url: 'https://www.mynavyexchange.com/browse/luggage-travel/_/N-300808423', aorOwner: 'Daryl' },
+  { label: 'Toys', url: 'https://www.mynavyexchange.com/browse/_/N-942274772', aorOwner: 'Daryl' },
+  { label: 'Household Essentials', url: 'https://www.mynavyexchange.com/browse/_/N-3864213259', aorOwner: 'Megan' },
+  { label: 'Health & Wellness', url: 'https://www.mynavyexchange.com/browse/_/N-513134447', aorOwner: 'Megan' },
+  { label: 'Pet', url: 'https://www.mynavyexchange.com/browse/_/N-1226486567', aorOwner: 'Maddie' },
+  { label: 'General Hardware', url: 'https://www.mynavyexchange.com/browse/general-hardware/_/N-4211009183', aorOwner: 'Daryl' },
+  { label: 'Food, Snacks & Candy', url: 'https://www.mynavyexchange.com/browse/_/N-328199723', aorOwner: 'Daryl' },
 ]
